be94d467429ccb15c0e24b65dabcbd2182557223
umutbardak <umtbrdk35@gmail.com>
2023-09-27 12:23:12 +0300

    guncel

--- lib/pages/welcomeScreen.dart
+++ lib/pages/welcomeScreen.dart
@@ -3,7 +3,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:lottie/lottie.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:url_launcher/url_launcher.dart';
 
